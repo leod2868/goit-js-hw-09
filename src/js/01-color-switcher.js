c8c5b3626f75f0d9654d02a29fe6ce83c46e7b48
Leonid <ztvlnd777@gmail.com>
2023-03-20 20:15:43 +0200

    ghb

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+let timerId = null;
 
+refs.startBtn.addEventListener('click', onSartBatton);
+refs.stopBtn.addEventListener('click', onStopBatton);
+
+function onSartBatton() {
+    refs.startBtn.setAttribute('disabled', true);
+    refs.stopBtn.removeAttribute('disabled', true);
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        refs.body.style.backgroundColor = color
+    }, 1000);
+}
+
+function onStopBatton() {
+    refs.stopBtn.setAttribute('disabled', true);
+    refs.startBtn.removeAttribute('disabled', true);
+    clearInterval(timerId);
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const refs = {
+  form : document.querySelector('.form')
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+refs.form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const delay = Number(refs.form.elements.delay.value);
+  const step = Number(refs.form.elements.step.value);
+  const amound = Number(refs.form.elements.amound.value);
+  
+  let position = 1;
+  let subDelay = delay;
+  for (let i = 0; i <= amound - 1; i += 1) {
+    position += i;
+    subDelay += step * i;
+
+    createPromise(position, subDelay)
+      .then(({ position, subDelay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${subDelay}ms`);
+  })
+  .catch(({ position, subDelay }) => {
+    console.log(`❌ Rejected promise ${position} in ${subDelay}ms`);
+  });
+}
+event.currentTarget.reset();
+}
+)
+
+
+
