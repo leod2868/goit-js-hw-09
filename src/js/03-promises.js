c64f89457f44ceb2db9677758a28a19051e9c66e
Leonid <ztvlnd777@gmail.com>
2023-03-20 22:49:32 +0200

    1..3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,7 +5,6 @@ const refs = {
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
@@ -15,25 +14,26 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
+    
 refs.form.addEventListener('submit', (event) => {
   event.preventDefault();
   const delay = Number(refs.form.elements.delay.value);
   const step = Number(refs.form.elements.step.value);
-  const amount = Number(refs.form.elements.amount.value);
+  const amount = Number(refs.form.elements.amount.value); 
   
-
+  let position = 1;
+  let subDelay = delay;
   
-  for (let i = 0; i <= (amount -1); i += 1) {
-    let subDelay = step * i + delay;
-    
+  for (let i = 0; i <= amount -1; i += 1) {
+    position += i;
+    subDelay += step * i;
 
-    createPromise( i, subDelay)
-      .then(({ i, subDelay }) => {
-    console.log(`✅ Fulfilled promise ${i} in ${subDelay}ms`);
+    createPromise( position, subDelay)
+    .then(({ position, subDelay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${subDelay}ms`);
   })
   .catch(({ position, subDelay }) => {
-    console.log(`❌ Rejected promise ${i} in ${subDelay}ms`);
+    console.log(`❌ Rejected promise ${position} in ${subDelay}ms`);
   });
 }
 event.currentTarget.reset();
