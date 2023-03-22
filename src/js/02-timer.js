eb0b31fd2e0da40c1435a9cb5df03780f449d9f9
Leonid <ztvlnd777@gmail.com>
2023-03-22 18:37:29 +0200

    1...3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,28 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
+
+    inputDate = document.querySelector('#datetime-picker');
+    startBtn = document.querySelector('button[data-start]');
+    daysRef = document.querySelector('span[data-days]');
+    hoursRef = document.querySelector('span[data-hours]');
+    minsRef = document.querySelector('span[data-minutes]');
+    secsRef = document.querySelector('span[data-seconds]');
+
+
+let timerId = null;
+
+startBtn.setAttribute('disabled', true);
+startBtn.addEventListener('click', onSartTimer);
+
+function flatpickr(inputDate, options);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+  console.log(selectedDates[0]);
+  },
+};
